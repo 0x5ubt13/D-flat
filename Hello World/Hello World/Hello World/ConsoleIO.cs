33bb8f1a4cccfadd5c932dabe881ced456e46d64
0x5utb13 <5ubt13@protonmail.com>
2024-04-30 16:57:54 +0100

    sync

--- Hello World/Hello World/Hello World/ConsoleIO.cs
+++ Hello World/Hello World/Hello World/ConsoleIO.cs
@@ -13,14 +13,27 @@ namespace Hello_World
             Console.WriteLine("Hello my name is Gabriel!");
 
             Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
+            string name = Console.ReadLine().Trim();
 
-            Console.WriteLine("Enter your age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine().Trim());
 
             Console.WriteLine($"Your name is {name}, and your age is {age}");
 
-            Console.ReadLine();
+            if (age <0 || age > 150)
+            {
+                Console.WriteLine("Error: invalid age.");
+                return;
+            }
+
+            if (age >= 18 && age <= 25) 
+            { 
+                Console.WriteLine("You are between 18 and 25");
+            }    
+            else if (age >= 26)
+            {
+                Console.WriteLine("You are older than 26");
+            }
         }
     }
 }
